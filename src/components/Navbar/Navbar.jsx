ce1a4a92a475c4a345b85a45de0d0387e9b904ba
IbrohimAsomov <ibrohimasomov7@gmail.com>
2022-06-28 20:44:57 +0500

    v0.2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,28 +8,28 @@ const Navbar = () => {
         <nav className="navbar bg-primary p-2 text-light">
           <ul className="w-25 d-flex justify-content-between list-unstyled align-items-center mt-2">
             <li>
-              <NavLink to="/home" className="text-light">
+              <NavLink to="/home" className="text-decoration-none text-light">
                 Home
               </NavLink>
             </li>
 
             <li>
-              <NavLink to="/aboute" className="text-light">
+              <NavLink to="/aboute" className="text-decoration-none text-light">
                 About
               </NavLink>
             </li>
             <li>
-              <NavLink to="/contant" className="text-light">
+              <NavLink to="/contant" className="text-decoration-none text-light">
                 Contact
               </NavLink>
             </li>
             <li>
-              <NavLink to="/login" className="text-light">
+              <NavLink to="/login" className="text-decoration-none text-light">
                 login
               </NavLink>
             </li>
             <li>
-              <NavLink to="/news" className="text-light">
+              <NavLink to="/news" className="text-decoration-none text-light">
                 News
               </NavLink>
             </li>
